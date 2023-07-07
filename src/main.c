7d6a67dc2c37becce61ea173996b344fdc43fc75
Toby Godfrey <toby.godfrey2003@gmail.com>
2023-07-07 15:33:23 -0400

    doxygen

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/**
+ * @file main.c
+ * @brief The entry point for testing the library
+ *
+ * @author Toby Godfrey
+ */
+
+#include <stdio.h>
+
+/**
+	* @brief The entry point for the program
+	*
+	* @returns An integer success value, non-zero values are failure codes
+	*/
+int main(){
+	printf("Hello World!\n");
+	
+	return 0;
+}
