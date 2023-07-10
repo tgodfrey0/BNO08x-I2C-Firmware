45c3ad5d93c05ac777abb289c981854ae7ea176a
Toby Godfrey <toby.godfrey2003@gmail.com>
2023-07-10 09:07:53 -0400

    no latex

--- docs/html/defs_8h.js
+++
@@ -1,6 +0,0 @@
-var defs_8h =
-[
-    [ "FREQ_HZ", "defs_8h.html#ac07f8b71ab75b4b60f9e7303aa98689b", null ],
-    [ "SCL_PIN", "defs_8h.html#a06c967e78bcedcee909a70764f879433", null ],
-    [ "SDA_PIN", "defs_8h.html#a526d580d324bce60a3e964066fae77e3", null ]
-];
--- docs/html/search/defines_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['freq_5fhz_0',['FREQ_HZ',['../defs_8h.html#ac07f8b71ab75b4b60f9e7303aa98689b',1,'defs.h']]]
-];
